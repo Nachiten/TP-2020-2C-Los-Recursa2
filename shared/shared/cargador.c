b4bd82dffcae4de8b58165bb8b17f139708857c2
Ignacio Baptista [Lubuntu] <ibaptista@est.frba.utn.edu.ar>
2020-12-07 17:22:08 -0300

    Cambiados todos los log para q sean info, no trace

--- shared/shared/cargador.c
+++ shared/shared/cargador.c
@@ -3,13 +3,13 @@
 //Deja un log listo para usar
 t_log* cargarUnLog(char* ruta, char* nombreLog)
 {
-	return log_create(ruta, nombreLog, 1, LOG_LEVEL_TRACE);
+	return log_create(ruta, nombreLog, 1, LOG_LEVEL_INFO);
 }
 
 //Deja un log listo para usar
 t_log* cargarUnLogApp(char* ruta, char* nombreLog)
 {
-	return log_create(ruta, nombreLog, 1, LOG_LEVEL_TRACE);
+	return log_create(ruta, nombreLog, 1, LOG_LEVEL_INFO);
 }
 
 t_log* cargarUnLogDeCliente(char* ruta, char* nombreLog)
