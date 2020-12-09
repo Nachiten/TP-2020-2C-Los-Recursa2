2c21ce5ecc6614b10b437f55b650b8fbe90e694b
Ignacio Baptista [Lubuntu] <ibaptista@est.frba.utn.edu.ar>
2020-12-09 16:44:07 -0300

    Modificados detalles en .sh

--- compilarModulos.sh
+++ compilarModulos.sh
@@ -1,29 +1,29 @@
 echo Compilando app...
-sleep 3
+sleep 1
 cd app/Debug
 make clean
 make all
 cd ../..
 echo Compilando cliente...
-sleep 3
+sleep 1
 cd cliente/Debug
 make clean
 make all
 cd ../..
 echo Compilando comanda...
-sleep 3
+sleep 1
 cd comanda/Debug
 make clean
 make all
 cd ../..
 echo Compilando restaurante...
-sleep 3
+sleep 1
 cd restaurante/Debug
 make clean
 make all
 cd ../..
 echo Compilando sindicato...
-sleep 3
+sleep 1
 cd sindicato/Debug
 make clean
 make all
